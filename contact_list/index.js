352f10c8ec13712d3d597de1e4162f7cd29a7e27
Puneet-iner <imhtcy312@gmail.com>
2020-06-08 16:15:30 +0530

    added delete icon in contact list

--- contact_list/index.js
+++ contact_list/index.js
@@ -13,7 +13,7 @@ var contactList = [
 app.set('view engine', 'ejs');
 //__dirname is used to get current directory
 app.set('views', path.join(__dirname, 'views'));// just to set views value to view path
-app.use(express.urlencoded({ extended: false }));//middleware method
+app.use(express.urlencoded({ extended: false }));//middleware method, not for params but is used for form data
 app.use(express.static("assets"));//used here to make middleware for accessing contents of assets folder => for seeint this effect just go and see the home.ejs file where css file and js script has been loded
 // app.use vs router.use -> https://stackoverflow.com/questions/27227650/difference-between-app-use-and-router-use-in-express
 
@@ -52,6 +52,29 @@ app.post('/create-contact', function(req, res){
     console.log(req.body.name);
     // return res.redirect('/');//rediredcts to an end point
     //here "back" has it's own significance
+    return res.redirect('back');
+});
+
+//delete-contact/10 => params
+//delete-contact/?phone=....&something=...
+// app.get('/delete-contact/:phone', function(req, res){
+//     console.log(req.params);
+//     let phone = req.params.phone;
+//     let contactIndex = contactList.findIndex(contact => contact.phone == phone);//it is a kind of for each loop  which is very advance
+//     if(contactIndex != -1){
+//         contactList.splice(contactIndex, 1);
+//     }
+//     return res.redirect('back');
+// });
+
+//query is like form, above method also works, it is like forms get =>it will show params in the loading link
+app.get('/delete-contact', function(req, res){
+    console.log(req.query);
+    let phone = req.query.phone;
+    let contactIndex = contactList.findIndex(contact => contact.phone == phone);//it is a kind of for each loop  which is very advance
+    if(contactIndex != -1){
+        contactList.splice(contactIndex, 1);
+    }
     return res.redirect('back');
 });
 
